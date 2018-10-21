7b66da25322b67b34dc38e379e92ce519ba4e436
Njarasoa M. J. Rakotozafy <raknjarasoa@gmail.com>
2018-10-21 16:54:39 +0300

    Add Routes mock and currencies module

--- src/app/currencies/currencies.module.ts
+++ src/app/currencies/currencies.module.ts
@@ -1,12 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppMaterialModule } from '../app-material.module';
+
 import { DisplayerComponent } from './displayer/displayer.component';
 import { OneCurrencyComponent } from './one-currency/one-currency.component';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    HttpClientModule,
+    AppMaterialModule
   ],
-  declarations: [DisplayerComponent, OneCurrencyComponent]
+  declarations: [
+    DisplayerComponent,
+    OneCurrencyComponent
+  ],
+  exports: [
+    DisplayerComponent
+  ]
 })
 export class CurrenciesModule { }
