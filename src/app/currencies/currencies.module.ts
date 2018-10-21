c32f0b7d7cd7c6536cb661e320f3605939a297d9
Njarasoa M. J. Rakotozafy <raknjarasoa@gmail.com>
2018-10-21 17:39:42 +0300

    Implements getAllCountry service

--- src/app/currencies/currencies.module.ts
+++ src/app/currencies/currencies.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppMaterialModule } from '../app-material.module';
+import { ApiModule } from '../api/api.module';
 
 import { DisplayerComponent } from './displayer/displayer.component';
 import { OneCurrencyComponent } from './one-currency/one-currency.component';
@@ -11,7 +12,9 @@ import { OneCurrencyComponent } from './one-currency/one-currency.component';
   imports: [
     CommonModule,
     HttpClientModule,
-    AppMaterialModule
+    
+    AppMaterialModule,
+    ApiModule
   ],
   declarations: [
     DisplayerComponent,
--- src/app/currencies/services/currencies.service.ts
+++ src/app/currencies/services/currencies.service.ts
@@ -1,9 +1,39 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams, HttpHeaders } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+import { catchError, map, tap } from 'rxjs/operators';
+
+import { ApiService } from '../../api/api.service';
+import { Country } from '../models';
+
+import { environment } from '../../../environments/environment';
+
+const API_ENDPOINT = `${environment.API_ENDPOINT}/all?fields=name;alpha2Code;alpha3Code;capital;region;subregion;population;latlng;currencies;flag`;
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Accept': 'application/json'
+  })
+};
 
 @Injectable({
   providedIn: 'root'
 })
 export class CurrenciesService {
 
-  constructor() { }
+  constructor(
+    private apiService: ApiService,
+    private http: HttpClient) { }
+
+  getAllCountry(): Observable<Country[]> {
+    return this.http.get<Country[]>(API_ENDPOINT, httpOptions)
+      .pipe(
+        map(res => {
+          debugger
+          return res['payload'];
+        }),
+        tap(TOTOOs => this.apiService.logInfo(TOTOOs)),
+        catchError(this.apiService.handleError('getAllCountry', []))
+      );
+  }
 }
